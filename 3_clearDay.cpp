dae337530a8bb73683084f5ec8cfa7641a6a9971
imran <zunaeede@gmail.com>
2024-11-14 17:49:25 +0600

    main - added another file.

--- 3_clearDay.cpp
+++ 3_clearDay.cpp
@@ -9,15 +9,3 @@ int main() {
 
 }
 
-// Double Rent
-// Chefina decided to move into Chef's apartment.
-// Chef was initially paying a rent of 
-// X
-// X rupees. Since Chefina is moving in, the owner decided to double the rent.
-
-// Find the final rent Chef needs to pay.
-
-// Explanation:
-// Chef was initially paying 10 10 rupees. After Chefina moves in, he needs to pay 
-// 2⋅ 10 = 20
-// 2⋅10 = 20 rupees.
