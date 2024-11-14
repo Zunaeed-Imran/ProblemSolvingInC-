fac4544b89902e6e18868915528718070e0e7c20
imran <zunaeede@gmail.com>
2024-11-14 17:51:34 +0600

    main - added explanation.

--- 3_clearDay.cpp
+++ 3_clearDay.cpp
@@ -9,3 +9,16 @@ int main() {
 
 }
 
+// Clear Day
+// Chef classifies a day to be either rainy, cloudy, or clear.
+
+// In a particular week, Chef finds 
+// X
+// X days to be rainy and 
+// Y
+// Y days to be cloudy.
+// Find the number of clear days in the week.
+
+// Explanation:
+// There are  7 days in a week. If there are 2 rainy days and  3 cloudy days, then the remaining
+// 7−2−3=2 days are clear.
